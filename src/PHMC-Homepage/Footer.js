c6cbbebf195a6ee60fd957044d57e6878f5e55a1
LeepDev <hyungy@gmail.com>
2020-12-31 13:35:02 -0600

    Email Policy Added

--- src/PHMC-Homepage/Footer.js
+++ src/PHMC-Homepage/Footer.js
@@ -17,7 +17,7 @@ class Footer extends React.Component {
               <Link to='/'>Home</Link> | Blog | <Link to='/contact'>Contact</Link> | <Link to='/locations'>Locations</Link> | <Link to='/careers'>Careers</Link> | Payment
               </div>
             <div className="nmlsPrivacyPolicyTermsOfUseDisclosureLicensing1">
-              NMLS | Privacy Policy | Terms of Use | <Link to='/ccpa'>CCPA</Link> | <Link to='/licensing'>Licensing</Link>
+              <Link to='/emailPolicy'>Email Policy</Link> | Privacy Policy | Terms of Use | <Link to='/ccpa'>CCPA</Link> | <Link to='/licensing'>Licensing</Link>
             </div>
             <div className="x2021PlatinumHomeMortgageCorporationAllRightsReservedLegalLicensingPrivacyInNewYorkDbaPhmcMortgageNmlsId1">© 2021 Platinum Home Mortgage Corporation. All rights reserved. Legal | Licensing | Privacy In New York: dba PHMC Mortgage NMLS ID #13589. For licensing information, go to: www.nmlsconsumeraccess.org All loans subject to credit approval and certain restrictions may apply. Information subject to change without notice. HomeStyle® is a registered trademark of Fannie Mae.</div>
             <div className="rectangle1111"></div>
