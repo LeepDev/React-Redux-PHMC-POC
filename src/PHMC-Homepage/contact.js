9076e0f62a64e3e3679fe3cbe039450819ebc1cd
hhuynh84 <44589229+hhuynh84@users.noreply.github.com>
2021-01-04 12:46:24 -0600

    contact form update

--- src/PHMC-Homepage/contact.js
+++ src/PHMC-Homepage/contact.js
@@ -1,39 +1,55 @@
 
 import React from "react";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import Header from './Header';
 import Footer2 from './Footer2';
 import "./css/contact.css";
+
 
 class Contact extends React.Component {
 
   constructor(props) {
       super(props);
-      this.state = {
+      this.state = {value: ''};
 
-      };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
   }
   
   render() {
     
     return (
         <div data-layer="dd354256-3ba7-4017-8459-17d06a9862da" className="contact">
+          <container className="contact">
           <div data-layer="c8889a80-696d-4845-816d-bf9e850461a7" className="rectangle4"></div>
           <div data-layer="f5f9a899-2a7f-4cfb-bfd0-afcb3c20dc87" className="rectangle3"></div>
           <Header></Header>
-          <div data-layer="9c16e6b2-6ab1-49f5-b2c1-3596ef90ba8b" className="rectangle14"></div>
-          <div data-layer="110ca9fd-80ea-4b95-b94c-32e96d68d649" className="rectangle15"></div>
-          <div data-layer="001749c3-8bb0-4983-8579-64055c950e8c" className="rectangle16"></div>
-          <div data-layer="e302f221-1de1-432e-bcfc-6f9f76283de7" className="rectangle74"></div>
-          <div data-layer="538087dd-f001-405a-8361-9c7d86b100d8" className="rectangle75"></div>
+         
           <div data-layer="2914dea1-aea2-42ed-bad9-5c8db4afe1b3" className="component72">            
             <svg data-layer="5d47f446-ff10-4edc-b496-7d2852be3655" preserveAspectRatio="none" viewBox="-1.5 -1.5 178 69" className="path10308d08f"><path d="M 0 0 L 175 0 L 175 66 L 0 66 L 0 0 Z"  /></svg>
-            <div data-layer="6f6c395e-91a0-44bd-ac3e-f009ec042425" className="send">Send</div>
+            <Button><div data-layer="6f6c395e-91a0-44bd-ac3e-f009ec042425" className="send">Send</div></Button>
           </div>
-          <div data-layer="1d8c1490-7b61-432b-96c0-f88ee63a6e9b" className="firstName">First Name</div>
-          <div data-layer="20f3ee1a-b51a-4cc0-beb6-018c1b80a756" className="lastName">Last Name</div>
-          <div data-layer="54f77743-c68d-4e36-8ffc-70bed1e3944e" className="emailAddress">Email Address</div>
-          <div data-layer="5fc76aaa-7fab-40a9-a870-b6af511e18a1" className="phoneNumber">Phone Number</div>
-          <div data-layer="17900b08-9388-430c-b948-e5b538061869" className="questioncomments">Question/Comments</div>
+
+<Form>
+<Form.Group controlId="formBasicEmail">
+    <Form.Control type="text" placeholder="First Name" className="firstNameInput"/>
+    <Form.Control type="text" placeholder="Last Name" className="LastNameInput"/>
+    <Form.Control type="email" placeholder="Email Address" className="emailAddressInput"/>
+    <Form.Control type="tel" placeholder="Phone Number" className="phoneNumberInput"/>
+    <Form.Control type="text" placeholder="Question/Comments" className="questionInput"/>
+  </Form.Group>
+</Form>
+
           <svg data-layer="34faea32-bb78-4dc4-a3c3-83c826a14707" preserveAspectRatio="none" viewBox="0 -1.5 1591 3" className="line13ed2b8ef"><path d="M 0 0 L 1591 0"  /></svg>
           <div data-layer="1bf11729-337d-40be-9cbd-6b23270ec5fa" className="component68">            
             <svg data-layer="7672b356-0b23-4064-8fb3-660a31bf2703" preserveAspectRatio="none" viewBox="-1.5 -1.5 178 69" className="path1fcce4837"><path d="M 0 0 L 175 0 L 175 66 L 0 66 L 0 0 Z"  /></svg>
@@ -49,7 +65,7 @@ class Contact extends React.Component {
           </div>
           <Footer2></Footer2>
           <div data-layer="3ef1ad91-80d3-470a-bd1e-86f47a4278f2" className="contactUs">Contact Us</div>
-
+          </container>
 </div>
 
 
