ba31507e3d05d78d2ecf34373cf029fb8f5f54f0
hhuynh84 <44589229+hhuynh84@users.noreply.github.com>
2021-01-04 17:25:31 -0600

    contact CSS alignment update

--- src/PHMC-Homepage/contact.js
+++ src/PHMC-Homepage/contact.js
@@ -5,6 +5,7 @@ import Button from 'react-bootstrap/Button';
 import Header from './Header';
 import Footer2 from './Footer2';
 import "./css/contact.css";
+import { Col, Container, Row } from "react-bootstrap";
 
 
 class Contact extends React.Component {
@@ -29,32 +30,34 @@ class Contact extends React.Component {
   render() {
     
     return (
-        <div data-layer="dd354256-3ba7-4017-8459-17d06a9862da" className="contact">
-          <container className="contact">
-          <div data-layer="c8889a80-696d-4845-816d-bf9e850461a7" className="rectangle4"></div>
-          <div data-layer="f5f9a899-2a7f-4cfb-bfd0-afcb3c20dc87" className="rectangle3"></div>
+      
+<Container>
+          <div data-layer="c8889a80-696d-4845-816d-bf9e850461a7" className="opacityLayer"></div>
+
           <Header></Header>
-            
+          <div className="contactUs">Contact Us</div>
+  
+  <div className="bodyAlign">
+  <div className="formAlign">
 <Form>
 <Form.Group controlId="formBasicEmail">
     <Form.Control type="text" placeholder="First Name" className="firstNameInput"/>
     <Form.Control type="text" placeholder="Last Name" className="LastNameInput"/>
     <Form.Control type="email" placeholder="Email Address" className="emailAddressInput"/>
     <Form.Control type="tel" placeholder="Phone Number" className="phoneNumberInput"/>
-    <Form.Control type="text" placeholder="Question/Comments" className="questionInput"/>
+    <textarea rows="4" cols="50" placeholder="Question/Comments" className="questionInput"/>
   </Form.Group>
 </Form>
 <Button className="send">Send</Button>
-
-<Button className="call">call</Button>
-<Button className="email">email</Button>
-<Button className="chat">chat</Button>
-          <Footer2></Footer2>
-          <div data-layer="3ef1ad91-80d3-470a-bd1e-86f47a4278f2" className="contactUs">Contact Us</div>
-          </container>
 </div>
-
-
+<div className="contactButtonAlign">
+<Button className="call">Call</Button>
+<Button className="email">Email</Button>
+<Button className="chat">Chat</Button>
+</div>
+</div>
+          <Footer2></Footer2>
+          </Container>
     );
   }
 }
