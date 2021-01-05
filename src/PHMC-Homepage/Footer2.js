08bd865af6fa2832b0bdf44578cf5d950c90f812
hhuynh84 <44589229+hhuynh84@users.noreply.github.com>
2021-01-05 15:35:07 -0600

    footer desktop reponsive update

--- src/PHMC-Homepage/Footer2.js
+++ src/PHMC-Homepage/Footer2.js
@@ -6,25 +6,28 @@ class Footer2 extends React.Component {
   render() {
     return (
 
-        <div>            
-            <div className="rectangle61"></div>
-            <div className="group4">
-                <div className="rectangle64"></div>
-                <div className="rectangle65"></div>
-                <div className="rectangle66"></div>
-            </div>
-            <div className="homeBlogContactLocationsCareersPayment">
+        <div className="footerStyle">            
+            <div className="platinumlogohorizontal"></div>
+            
+            <div className="rowOne">
+            <div className="rowTwo">
+            <div className="mainLinks">
               <Link to='/'>Home</Link> | Blog | <Link to='/contact'>Contact</Link> | <Link to='/locations'>Locations</Link> | <Link to='/careers'>Careers</Link> | Payment
               </div>
-            <div className="nmlsPrivacyPolicyTermsOfUseDisclosureLicensing">
+              <div className="contactIcon">
+                <div className="emailIcon"></div>
+                <div className="callIcon"></div>
+                <div className="chatIcon"></div>
+            </div>
+            <div className="legalLinks">
               <Link to='/emailPolicy'>Email Policy</Link> | Privacy Policy | Terms of Use | <Link to='/ccpa'>CCPA</Link> | <Link to='/licensing'>Licensing</Link>
             </div>
-            <div className="x2021PlatinumHomeMortgageCorporationAllRightsReservedLegalLicensingPrivacyInNewYorkDbaPhmcMortgageNmlsId">© 2021 Platinum Home Mortgage Corporation. All rights reserved. Legal | Licensing | Privacy In New York: dba PHMC Mortgage NMLS ID #13589. For licensing information, go to: www.nmlsconsumeraccess.org All loans subject to credit approval and certain restrictions may apply. Information subject to change without notice. HomeStyle® is a registered trademark of Fannie Mae.</div>
-            <div className="rectangle67"></div>
-            <div className="rectangle68"></div>
-            <svg preserveAspectRatio="none" viewBox="0 -0.5 1560 1" className="line1"><path d="M 0 0 L 1560 0"  /></svg>
-            <div className="rectangle69"></div>
-            <div className="platinumlogohorizontal"></div>
+            </div>
+            <div className="legalfooter">© 2021 Platinum Home Mortgage Corporation. All rights reserved. Legal | Licensing | Privacy In New York: dba PHMC Mortgage NMLS ID #13589. For licensing information, go to: www.nmlsconsumeraccess.org All loans subject to credit approval and certain restrictions may apply. Information subject to change without notice. HomeStyle® is a registered trademark of Fannie Mae.</div>
+            </div>
+            
+            <div className="EHOlogo"></div>
+            <div className="BBBlogo"></div>
         </div>
 
     );
